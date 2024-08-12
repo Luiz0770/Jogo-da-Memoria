0497ee5a89c04b29daa441c3ccd77b342f9ac216
Luiz0770 <ramosluizfelipe104@gmail.com>
2024-08-12 01:09:05 -0300

    Adicionando a funcionalidade de ramdomizacao

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const imagens = document.querySelectorAll('.imagem')
 let listaDeSelecionador = [];
+let listaOpcoes = [
+    'magrao', 'magrao',
+    'bolivia', 'bolivia',
+    'vitor', 'vitor',
+    'caio', 'caio',
+    'luiz', 'luiz',
+    'rippi', 'rippi',
+]
+
+exibirNaTela()
+
+const imagens = document.querySelectorAll('.imagem');
 
 imagens.forEach((imagem) => {
     imagem.addEventListener('click', () => {
@@ -10,33 +21,58 @@ imagens.forEach((imagem) => {
         imagem.classList.remove("desativada")
         imagem.classList.add(`${id}`)
 
-        setTimeout(function () {
-            verificarOpcao(id) 
-        }, 1000)
+        if (listaDeSelecionador.length == 2) {
+            setTimeout(function () {
+                verificarOpcao(id)
+            }, 1000)
+        }
     })
 })
 
 function verificarOpcao(id) {
-    if (listaDeSelecionador.length == 2) {
-        if (listaDeSelecionador[0] == listaDeSelecionador[1]) {
-            alert('Acertou')
-            const opcoes = document.querySelectorAll(`#${listaDeSelecionador[0]}`)
-            opcoes.forEach((opcao) => {
-                opcao.remove();
-            })
-            listaDeSelecionador =[];
-        }
-        else {
-            alert('Errou')
-            listaDeSelecionador =[];
-            const opcoes = document.querySelectorAll('.imagem-selecionada')
-            console.log(opcoes)
-            opcoes.forEach((opcao) => {
-                opcao.classList.remove("imagem-selecionada")
-                opcao.classList.remove(`${id}`)
-                opcao.classList.add("desativada")
-                console.log(opcao)
-            })
-        }
+    if (listaDeSelecionador[0] == listaDeSelecionador[1]) {
+        alert('Acertou')
+        const opcoes = document.querySelectorAll(`#${listaDeSelecionador[0]}`)
+        opcoes.forEach((opcao) => {
+            opcao.remove();
+        })
+        listaDeSelecionador = [];
+        console.log(imagens)
     }
+    else {
+        alert('Errou')
+        listaDeSelecionador = [];
+        const opcoes = document.querySelectorAll('.imagem-selecionada')
+        console.log(opcoes)
+        opcoes.forEach((opcao) => {
+            opcao.classList.remove("imagem-selecionada")
+            opcao.classList.remove(`${id}`)
+            opcao.classList.add("desativada")
+            console.log(opcao)
+        })
+    }
+}
+
+function embaralharOpcoes() {
+    for (let i = listaOpcoes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [listaOpcoes[i], listaOpcoes[j]] = [listaOpcoes[j], listaOpcoes[i]];
+    }
+    return listaOpcoes;
+}
+
+function exibirNaTela() {
+    embaralharOpcoes()
+    const container = document.querySelector('.container')
+    for (let i = 0; i < listaOpcoes.length; i++ ) {
+        container.appendChild(criarOpcao(i))
+    }
+}
+
+function criarOpcao(i) {
+    const button = document.createElement('button')
+    button.classList.add("imagem")
+    button.classList.add("desativada")
+    button.id = listaOpcoes[i]
+    return button
 }
